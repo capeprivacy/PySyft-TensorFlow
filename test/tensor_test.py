e0113251d826b4c719ba0444bbb6407c274c9fb6
Ben DeCoste <bendecoste@gmail.com>
2019-10-03 10:49:50 -0300

    dsa

--- test/tensor_test.py
+++ test/tensor_test.py
@@ -9,10 +9,3 @@ def test_send_get_constant(remote):
     x_ptr = x_to_give.send(remote)
     x_gotten = x_ptr.get()
     assert tf.math.equal(x_to_give, x_gotten)
-
-def test_add(remote):
-  x = tf.constant([3.0, 3.0]).send(remote)
-  y = tf.constant([2.0, 2.0]).send(remote)
-
-  z = x + y
-  assert z.get().equal(tf.constant([5.0, 5.0]))
